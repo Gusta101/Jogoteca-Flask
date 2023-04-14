eea3eca4c8ae051dfc6ef6c8328449e378deeba7
Gusta101 <gu_gui14@hotmail.com>
2023-04-14 11:52:11 -0300

    'LOGIN/LOGOUT' e dados em 'cookies'(session)

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -13,6 +13,11 @@ jogo4 = Jogo('GTA', 'Aventura', 'Xbox')
 lista = [jogo1, jogo2, jogo3, jogo4]
 
 app = Flask(__name__)
+app.secret_key = 'paçoca'
+
+# /lista.html   --> titulo (Título da página)
+#                   jogos (Lista de objetos Jogo a serem listados)
+# /novo.html    --> titulo (Título da página)
 
 @app.route('/')
 def index():
@@ -29,6 +34,26 @@ def criar():
     console = request.form['console']
     jogo_novo = Jogo(nome, categoria, console)
     lista.append(jogo_novo)
+    return redirect('/')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if request.form['senha'] == 'alohomora':
+        session['usuario_logado'] = request.form['usuario']
+        flash(session['usuario_logado'] + ' logado com sucesso!')
+        return redirect('/')
+    else:
+        flash('Usuário não logado')
+        return redirect('/login')
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
     return redirect('/')
 
 app.run(port=8080, debug=True)
