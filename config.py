ba8d863738d8a9985ba01ad37d6505245783bb02
Gusta101 <gu_gui14@hotmail.com>
2023-04-18 15:19:30 -0300

    Reestruturação e CRUD completo(delete e update implementados)

---
+++ config.py
@@ -0,0 +1,10 @@
+SECRET_KEY = 'paçoca'
+
+SQLALCHEMY_DATABASE_URI = \
+    '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
+        SGBD = 'mysql+mysqlconnector',
+        usuario = 'root',
+        senha = 'admin',
+        servidor = '127.0.0.1',
+        database = 'jogoteca'
+    )
